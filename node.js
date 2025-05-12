da137802ca0e7c5831f3b9bc10e865f2a2b5f764
pile23 <pile23@proton.me>
2025-05-12 20:17:47 +0200

    Add files via upload

---
+++ node.js
@@ -0,0 +1,15 @@
+const http = require('node:http');
+const fs = require('node:fs');
+
+http
+		.createServer((request, response) => {
+				const { headers, method, url } = request;
+				response.statusCode = 200;
+				response.setHeader('Content-Type', 'text/html');
+				fs.readFile('index.html', (err, data)=>{
+						response.end(data);
+				});
+
+				
+		})
+		.listen(8080);
